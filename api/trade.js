dba1b6039fc8057dd1a9cf5027ad30499da30493
firstouch24 <firsttouchhq01@gmail.com>
2024-10-22 14:28:07 +0800

    Update trade.js

--- api/trade.js
+++ api/trade.js
@@ -1,4 +1,4 @@
 module.exports = async (req, res) => {
-    var response = {"status":"ok","msg":"","data":{"symbol":"XAUUSD","trade":"BUY","priceStart":2730.5,"priceEnd":2728.3,"SL":2720}};
+    var response = {"status":"ok","msg":"","data":{"symbol":"XAUUSD","trade":"BUY","priceStart":2733.5,"priceEnd":2736.3,"SL":2730}};
     return res.status(200).json(response);
 }
